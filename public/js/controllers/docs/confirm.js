55e4106d1f39c14bbb643727377dbe3d37ab4bb1
Heinrich Löwen <loewen.heinrich@gmail.com>
2016-05-20 22:56:46 +0200

    code cleanup

--- public/js/controllers/docs/confirm.js
+++ public/js/controllers/docs/confirm.js
@@ -7,22 +7,22 @@ app.controller("DocConfirmController", function($scope, $routeParams, $location,
     // API
     $scope.loadData = function() {
         $docService.get($routeParams.doc_id).success(function(response) {
-            console.log("Check confirm get");
             $scope.doc = response;
+        });
+    };
+
+    $scope.generatePDF = function() {
+        $docService.pdf($routeParams.doc_id).success(function(response) {
             $scope.icf_eng = angular.copy($scope.doc._id + "_eng.pdf");
             $scope.icf_ger = angular.copy($scope.doc._id + "_ger.pdf");
         });
-        // Call pdf generation function
-        $docService.pdf($routeParams.doc_id).success(function(response) {
-          console.log("Check confirm pdf");
-        });
-
-    };
+    }
 
     // INIT
     $scope.loadData();
+    $scope.generatePDF();
     $scope.validateEthics = function() {
-      return false
+        return false
     };
 
 });
