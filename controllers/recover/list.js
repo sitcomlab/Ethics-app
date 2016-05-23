428e4e32cd4816647308e5f240a151d5aedb22c3
Heinrich Loewen <loewen.heinrich@gmail.com>
2016-05-23 16:05:09 +0200

    bugfixes

--- controllers/recover/list.js
+++ controllers/recover/list.js
@@ -11,57 +11,58 @@ var mustache = require('mustache');
 
 
 // LIST
-exports.request = function(req, res){
+exports.request = function(req, res) {
 
     // Find all documents
-	Doc.find( { email_address: req.params.email_address } ).exec(function(err, docs) {
+    Doc.find({
+        email_address: req.params.email_address
+    }).exec(function(err, docs) {
 
-		var _doc = {};
-		_doc.docs = docs;
-		_doc.first_name = "Nicho"; // TODO: _doc.first_name
-		_doc.last_name = "S."; // TODO: _doc.last_name
+        var _doc = {};
+        _doc.docs = docs;
 
-		// Read Template
-		fs.readFile(path.join(__dirname, '../../templates/email/recover.html'), function (err, data) {
-			if (err) throw err;
+        // Read Template
+        fs.readFile(path.join(__dirname, '../../templates/email/recover.html'), function(err, data) {
+            if (err) throw err;
 
-			// Render HTML-content
-			var output = mustache.render(data.toString(), _doc);
+            // Render HTML-content
+            var output = mustache.render(data.toString(), _doc);
 
-			// Create Text for Email-Previews and Email without HTML-support
-			var lines = "";
-			for(var i=0; i<docs.length; i++) {
-				lines = lines + "* " + docs[i].project_name.toString() + ": " + docs[i]._id.toString() + "\n";
-			}
-			var text =
-				'Hello ' + _doc.first_name.toString() + ' ' + _doc.last_name.toString() + ',\n' +
-				'You asked for your Document-IDs. Please find a list below with all of your Projects related to this Email-Address.\n\n' +
-				lines + '\n\n\n' +
-				'Ethics-App by Institute for Geoinformatics (Heisenbergstraße 2, 48149 Münster, Germany';
+            // Create Text for Email-Previews and Email without HTML-support
+            var lines = "";
+            for (var i = 0; i < docs.length; i++) {
+                lines = lines + "* " + docs[i].project_name.toString() + ": " + docs[i]._id.toString() + "\n";
+            }
 
-			// Set Mail options
-			var mailOptions = {
-				from: _mailOptions.from,
-				to: req.params.email_address,
-				subject: 'You asked for your Document-IDs',
-				text: text,
-				html: output
-			};
+            var text =
+                'Hello ' + docs[0].first_name.toString() + ' ' + docs[0].last_name.toString() + ',\n' +
+                'You asked for your Document-IDs. Please find a list below with all of your Projects related to this Email-Address.\n\n' +
+                lines + '\n\n\n' +
+                'Ethics-App by Institute for Geoinformatics (Heisenbergstraße 2, 48149 Münster, Germany';
 
-			// Send Email
-			transporter.sendMail(mailOptions, function(error, info){
-				if(error){
-					return console.log(error);
-				} else {
-					console.log('Message sent: ' + info.response);
-				}
-			});
-		});
+            // Set Mail options
+            var mailOptions = {
+                from: _mailOptions.from,
+                to: req.params.email_address,
+                subject: 'You asked for your Document-IDs',
+                text: text,
+                html: output
+            };
 
-		// Send results
-		//console.log(docs);
-		//res.jsonp(docs);
-		res.send();
+            // Send Email
+            transporter.sendMail(mailOptions, function(error, info) {
+                if (error) {
+                    return console.log(error);
+                } else {
+                    console.log('Message sent: ' + info.response);
+                }
+            });
+        });
 
-	});
+        // Send results
+        //console.log(docs);
+        //res.jsonp(docs);
+        res.send();
+
+    });
 };
