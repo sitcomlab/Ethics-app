dec74cc8876aa5869490a2efdd774f68cb2a53bf
Heinrich Loewen <loewen.heinrich@gmail.com>
2016-05-25 16:57:52 +0200

    testing changes

--- public/js/controllers/docs/edit.js
+++ public/js/controllers/docs/edit.js
@@ -2,42 +2,23 @@ var app = angular.module("ethics-app");
 
 // Custom Directive
 app.directive('showErrors', function() {
-
     return {
-
-      restrict: 'A',
-
-      require: '^form',
-
-      link: function (scope, el, attrs, formCtrl) {
-
-        // find the text box element, which has the 'name' attribute
-
-        var inputEl   = el[0].querySelector("[name]");
-
-        // convert the native text box element to an angular element
-
-        var inputNgEl = angular.element(inputEl);
-
-        // get the name on the text box
-
-        var inputName = inputNgEl.attr('name');
-
-
-
-        // only apply the has-error class after the user leaves the text box
-
-        inputNgEl.bind('blur', function() {
-
-          el.toggleClass('has-danger', formCtrl[inputName].$invalid);
-
-        })
-
-      }
-
+        restrict: 'A',
+        require: '^form',
+        link: function(scope, el, attrs, formCtrl) {
+            // find the text box element, which has the 'name' attribute
+            var inputEl = el[0].querySelector("[name]");
+            // convert the native text box element to an angular element
+            var inputNgEl = angular.element(inputEl);
+            // get the name on the text box
+            var inputName = inputNgEl.attr('name');
+            // only apply the has-error class after the user leaves the text box
+            inputNgEl.bind('blur', function() {
+                el.toggleClass('has-danger', formCtrl[inputName].$invalid);
+            })
+        }
     }
-
-  });
+});
 
 // PUT
 app.controller("DocEditController", function($scope, $rootScope, $routeParams, $location, $docService, $window, $ngBootbox) {
