298570cefdd79434d99d544357ea0956250142ff
Nicho <nicho90@web.de>
2016-12-22 22:27:00 +0100

    Schema fixes

--- sql/queries/documents/list_by_user.sql
+++ sql/queries/documents/list_by_user.sql
@@ -1,4 +1,11 @@
-SELECT *
+SELECT
+    document_id,
+    created,
+    to_char(created, 'YYYY-MM-DD HH24:mm:ss') AS _created,
+    updated,
+    user_id,
+    document_title,
+    status
 FROM Documents
 WHERE user_id=$1::INTEGER
-ORDER BY document_title;
+ORDER BY created DESC;
