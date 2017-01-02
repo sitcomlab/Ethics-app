95d67844791fc75b00857c9919d181125a93b7e5
Nicho <nicho90@web.de>
2017-01-02 20:06:59 +0100

    Redesigned schema for descriptions

--- sql/schema/concern_comments.sql
+++ sql/schema/concern_comments.sql
@@ -1,16 +1,15 @@
-DROP TABLE IF EXISTS Comments CASCADE;
+DROP TABLE IF EXISTS Concern_comments CASCADE;
 
 -- SCHEMA
-CREATE TABLE Comments (
+CREATE TABLE Concern_comments (
 
     -- General
-    comment_id SERIAL PRIMARY KEY,
+    concern_comment_id SERIAL PRIMARY KEY,
     created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
 
     -- Attributes
     document_id CHARACTER VARYING(255) NOT NULL REFERENCES Documents(document_id) ON UPDATE CASCADE ON DELETE CASCADE,
-    question SMALLINT NOT NULL CHECK (question >= 0),
     user_id INTEGER NOT NULL REFERENCES Users(user_id) ON UPDATE CASCADE ON DELETE SET NULL,
+    question SMALLINT NOT NULL CHECK (question >= 0),
     comment CHARACTER VARYING(255) NOT NULL
-
 );
--- sql/schema/descriptions.sql
+++ sql/schema/descriptions.sql
@@ -8,10 +8,15 @@ CREATE TABLE Descriptions (
     created TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
 
     -- Attributes
-    document_id CHARACTER VARYING(255) NOT NULL REFERENCES Documents(document_id) ON UPDATE CASCADE ON DELETE CASCADE,
     revision_id INTEGER NOT NULL REFERENCES Revisions(revision_id) ON UPDATE CASCADE ON DELETE CASCADE,
     language languages NOT NULL,
-    question SMALLINT NOT NULL CHECK (question >= 1),
-    value CHARACTER VARYING(255) NOT NULL
+    title CHARACTER VARYING(255) DEFAULT NULL,          -- 1st question
+    researcher CHARACTER VARYING(255) DEFAULT NULL,     -- 2nd question
+    study_time CHARACTER VARYING(255) DEFAULT NULL,     -- 3rd question
+    purpose CHARACTER VARYING(255) DEFAULT NULL,        -- 4th question
+    procedure CHARACTER VARYING(255) DEFAULT NULL,      -- 5th question
+    duration CHARACTER VARYING(255) DEFAULT NULL,       -- 6th question
+    risks CHARACTER VARYING(255) DEFAULT NULL,          -- 7th question
+    benefits CHARACTER VARYING(255) DEFAULT NULL        -- 8th question
 
 );
