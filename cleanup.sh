6c9de62b9a0db7b5e6617afa296c330bc7f872c3
SpeckiJ <SpeckiJ@gmail.com>
2017-03-31 17:40:23 +0200

    added cleanup script for automatic Document deletion after 7 day period. Script needs to be configured with the path to the documents folder.

---
+++ cleanup.sh
@@ -0,0 +1,8 @@
+
+cd /path/to/temp/doc
+
+for i in 1 2 3 4 5 6 7
+do
+regex_pastdays="$regex_pastdays$(date -d @$(expr $(date +'%s') \- $(expr $i \* 86400))  +'|%Y-%m-%d')"
+done
+find . -type 'd' | ( grep -v -E $(date +'%Y-%m-%d')${regex_pastdays} | grep -E '[0-9]' ) |  xargs rm -rf
