30bd398367cec605312f052d173c975eda2864c3
Nicho <nicho90@web.de>
2017-04-19 17:53:19 +0200

    Typo-fix

--- controllers/working_groups/get.js
+++ controllers/working_groups/get.js
@@ -32,7 +32,7 @@ exports.request = function(req, res) {
         function(client, done, callback) {
             // Database query
             client.query(query_get_group, [
-                req.params.group_id
+                req.params.working_group_id
             ], function(err, result) {
                 done();
                 if (err) {
