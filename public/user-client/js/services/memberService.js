bc486a3c2bb894eb339cca499bf6577511208a75
Nicho <nicho90@web.de>
2017-04-24 16:34:18 +0200

    Added pagination for members in user-client

--- public/user-client/js/services/memberService.js
+++ public/user-client/js/services/memberService.js
@@ -5,19 +5,66 @@ var app = angular.module("memberService", []);
 app.factory('$memberService', function($http, $log, config, $authenticationService) {
 
     var members;
+    var filter = {
+        offset: 0,
+        limit: 50,
+        former: false
+    };
+    var full_count = 0;
 
     return {
         get: function(){
             return members;
         },
-        getByStatus: function(status){
-            return _.where(members, { former: status });
+        getFilter: function(){
+            return filter;
+        },
+        getCount: function(){
+            return full_count;
         },
         set: function(data){
             members = data;
         },
-        list: function() {
-            return $http.get(config.apiURL + "/members", {
+        setFilter: function(data) {
+            filter = data;
+        },
+        setCount: function(data) {
+            full_count = data;
+        },
+        list: function(filter) {
+            var query = "?offset=" + filter.offset + "&limit=" + filter.limit + "&former=" + filter.former + "&";
+
+            // TODO: Add orderby
+
+            query = query.slice(0, -1);
+
+            return $http.get(config.apiURL + "/members" + query, {
+                headers: {
+                    'Authorization': 'Bearer ' + $authenticationService.getToken()
+                }
+            });
+        },
+        listByUniversity: function(university_id, filter) {
+            var query = "?former=" + filter.former + "&";
+
+            // TODO: Add orderby
+
+            query = query.slice(0, -1);
+
+            return $http.get(config.apiURL + "/universities/" + university_id + "/members" + query, {
+                headers: {
+                    'Authorization': 'Bearer ' + $authenticationService.getToken()
+                }
+            });
+        },
+        listByInstitute: function(institute_id, filter) {
+            var query = "?former=" + filter.former + "&";
+
+            // TODO: Add orderby
+
+            query = query.slice(0, -1);
+
+            return $http.get(config.apiURL + "/institutes/" + institute_id + "/members" + query, {
                 headers: {
                     'Authorization': 'Bearer ' + $authenticationService.getToken()
                 }
