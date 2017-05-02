79d787b02221ba30e52909e4dc54e66b1a63fa6a
Nicho <nicho90@web.de>
2017-05-02 16:21:24 +0200

    Improved templates

--- public/member-client/js/services/documentsService.js
+++ public/member-client/js/services/documentsService.js
@@ -9,7 +9,8 @@ app.factory('$documentsService', function($http, $log, config, $authenticationSe
         offset: 0,
         limit: 50,
         orderby: "created.desc",
-        document_status: "3"
+        course: "false",
+        status: "3"
     };
     var full_count = 0;
 
@@ -36,18 +37,21 @@ app.factory('$documentsService', function($http, $log, config, $authenticationSe
             var query = "?orderby=" + filter.orderby + "&";
 
             if(filter.offset && filter.offset !== null){
-                query = query + "&offset=" + filter.offset + "&";
+                query = query + "offset=" + filter.offset + "&";
             }
             if(filter.limit && filter.limit !== null){
-                query = query + "&limit=" + filter.limit + "&";
+                query = query + "limit=" + filter.limit + "&";
             }
-            if(filter.document_status !== null){
-                query = query + "status=" + filter.document_status + "&";
-            } else {
-                query = query + "";
+            if(filter.course !== null){
+                query = query + "course=" + filter.course + "&";
+            }
+            if(filter.status !== null){
+                query = query + "status=" + filter.status + "&";
             }
 
             query = query.slice(0, -1);
+
+            console.log(config.apiURL + "/documents" + query);
 
             return $http.get(config.apiURL + "/documents" + query, {
                 headers: {
@@ -59,13 +63,15 @@ app.factory('$documentsService', function($http, $log, config, $authenticationSe
             var query = "?orderby=" + filter.orderby + "&";
 
             if(filter.offset && filter.offset !== null){
-                query = query + "&offset=" + filter.offset + "&";
+                query = query + "offset=" + filter.offset + "&";
             }
             if(filter.limit && filter.limit !== null){
-                query = query + "&limit=" + filter.limit + "&";
+                query = query + "limit=" + filter.limit + "&";
             }
 
             query = query.slice(0, -1);
+
+            console.log(config.apiURL + "/courses/" + course_id + "/documents" + query);
 
             return $http.get(config.apiURL + "/courses/" + course_id + "/documents" + query, {
                 headers: {
@@ -77,13 +83,15 @@ app.factory('$documentsService', function($http, $log, config, $authenticationSe
             var query = "?orderby=" + filter.orderby + "&";
 
             if(filter.offset && filter.offset !== null){
-                query = query + "&offset=" + filter.offset + "&";
+                query = query + "offset=" + filter.offset + "&";
             }
             if(filter.limit && filter.limit !== null){
-                query = query + "&limit=" + filter.limit + "&";
+                query = query + "limit=" + filter.limit + "&";
             }
 
             query = query.slice(0, -1);
+
+            console.log(config.apiURL + "/users/" + user_id + "/documents" + query);
 
             return $http.get(config.apiURL + "/users/" + user_id + "/documents" + query, {
                 headers: {
