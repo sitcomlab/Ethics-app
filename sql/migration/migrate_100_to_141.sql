820a03c7cfd59ee689fd745fc561b1191b482023
Nicho <nicho90@web.de>
2018-07-01 20:21:15 +0200

    Fixed typos

--- sql/migration/migrate_100_to_141.sql
+++ sql/migration/migrate_100_to_141.sql
@@ -1,9 +1,8 @@
 -- Add to Concerns
-ALTER TABLE Concerns ADD q14_value boolean default null;
-ALTER TABLE Concerns ADD q14_explanation text default null;
-ALTER TABLE Concerns ADD q14_filename text default null;
-ALTER TABLE Concerns ADD q14_filepath text default null;
+ALTER TABLE Concerns ADD q14_value BOOLEAN DEFAULT NULL;
+ALTER TABLE Concerns ADD q14_explanation TEXT DEFAULT NULL;
+ALTER TABLE Concerns ADD q14_filename TEXT DEFAULT NULL;
+ALTER TABLE Concerns ADD q14_filepath TEXT DEFAULT NULL;
 
 -- Add to Comments
-
-ALTER TABLE Comments ADD q14_comment text default null;
+ALTER TABLE Comments ADD q14_comment TEXT DEFAULT NULL;
